6d451b4b9f7472d6abc46406aa83460fe3ad9155
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-09-06 19:51:04 +0100

    Styling the affiliate page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Body from "./components/Body";
 import "./index.css";
+import Affiliate from "./components/Affiliate";
 function App() {
   return (
     <>
       <div className="app">
         <Header />
         <Body />
+        <Affiliate />
       </div>
     </>
   );
---
+++ vite-project/src/components/Affiliate.tsx
@@ -0,0 +1,45 @@
+const Affiliate = () => {
+  return (
+    <>
+      <main className="affiliate-main">
+        <section className="affiliate-section">
+          <h2>
+            Mr. A owns a company and offers several useful products for
+            consumers. Mr. A offers to pay certain commissions to anyone who
+            sells his product and Mr. B accepts to do this. Mr. B is an
+            Affiliate. You can sign up as an Affiliate on Expernaire
+          </h2>
+          <h2>
+            Let me simplify it a bit. A friend of yours (let me say for example)
+            bola wants to buy a phone and you know another friend Ade that sells
+            gadgets. then you tell bola that you know A guy named Ade and he
+            sells phones , then Bola bought the phone from Ade , what just
+            happened is known as Affiliate marketing. You just performed
+            affiliate marketing the only difference is that you were not giving
+            a commision from bringing a buyer named Bola.
+          </h2>
+        </section>
+        <h2>There are two types of Affiliate marketing</h2>
+        <ul>
+          <li>Physical Marketing</li>
+          <li>Digital Marketing</li>
+        </ul>
+        <div className="digital-market">
+          <h2>
+            In the example i gave earlier that is known as Physical Affilite
+            marketing. But in this discourse , we would be talking about Digital
+            marketing and what exactly is it, How do i get the Products?
+          </h2>
+          <h3>
+            Digital products are online products that are like books , courses
+            and the likes. Now to answer the question how do i get this product,
+            apps like stakecut digistem provide product for you to promote and
+            they are free you don't need to pay for registration
+          </h3>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Affiliate;
