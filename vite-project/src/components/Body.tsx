c5142c457a3be80e6fd959819e8886c7923eb447
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-09-06 19:12:08 +0100

    Finished body page

---
+++ vite-project/src/components/Body.tsx
@@ -0,0 +1,49 @@
+import Total from "../assets/totalEarnings.jpg";
+const Body = () => {
+  return (
+    <>
+      <main className="main-body">
+        <h2>
+          But Before i proceed further, i would like to do a brief introduction
+          about myself and how i got here.
+        </h2>
+        <section className="text-body">
+          <h2>
+            My name is Ejiro and i am a unilag 200 level student of the
+            department of Applied Physics with electronics.
+          </h2>
+          <h3>
+            Asides being a busy student and Full MERN stack developer or you may
+            just call me a programmer. As a student i searched for how to make
+            an extra income online with just my phone (though i'm interning as a
+            developer) and realy busy i couldn't keep sustaining myself on
+            little pay grades , i came across various ponzi scheme platforms
+            those platforms that would say refer you friends to do this to do
+            that, they are all scam, i would like to mention their names but for
+            copyright issues i wont.
+          </h3>
+          <h3>
+            You might ask yourself, what i'm i getting at?. We all have the
+            notion that online businesses are scam and i was once like that too.
+            So two month's ago i met a friend and he introduced me to affiliate
+            marketing, you already know what i told him, your guess is as good
+            as mine i told him that it was a scam and that i am not interested.
+            But this guy kept engaging me with his status and i was keeping
+            tracks of his progress on all social media, i got overwhelmed then i
+            made the bold decision and started off. To my suprise in my first
+            two weeks as an affiliate marketer i made two sales. As of the 4th
+            of septembe when i'm creating this i have made a total of 150
+            dollars and i'm sure i'd have made more sales by the time you are
+            reading this.
+          </h3>
+        </section>
+        <div className="total-img">
+          <img src={Total} alt="" />
+        </div>
+        <h1>So what is Affiliate marketing?</h1>
+      </main>
+    </>
+  );
+};
+
+export default Body;
